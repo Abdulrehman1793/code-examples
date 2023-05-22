23b5befa408bb0034b8ee119469f0bf7beb52261
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-05-22 23:22:34 +0530

    Implemented acknowledge for rabbitmq message

--- spring-boot/rabbitmq-example/src/main/java/com/abdulrehman1793/rabbitmqexample/config/MessagingConfig.java
+++ spring-boot/rabbitmq-example/src/main/java/com/abdulrehman1793/rabbitmqexample/config/MessagingConfig.java
@@ -1,12 +1,18 @@
 package com.abdulrehman1793.rabbitmqexample.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.api.RabbitListenerErrorHandler;
+import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 public class MessagingConfig {
@@ -40,5 +46,37 @@ public class MessagingConfig {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(factory);
         rabbitTemplate.setMessageConverter(converter());
         return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setMessageConverter(converter());
+        return factory;
+    }
+
+    @Bean
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(5000); // Retry after 5 seconds
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(3); // Retry up to 3 times
+        retryTemplate.setRetryPolicy(retryPolicy);
+
+        return retryTemplate;
+    }
+
+    @Bean
+    public RabbitListenerErrorHandler errorHandler(RabbitTemplate rabbitTemplate) {
+        return (amqpMessage, message, exception) -> {
+            // Handle the error and decide whether to acknowledge or reject the message
+            throw new ListenerExecutionFailedException("Error occurred during message processing", exception);
+        };
     }
 }
--- spring-boot/rabbitmq-example/src/main/java/com/abdulrehman1793/rabbitmqexample/consumer/User.java
+++ spring-boot/rabbitmq-example/src/main/java/com/abdulrehman1793/rabbitmqexample/consumer/User.java
@@ -2,13 +2,35 @@ package com.abdulrehman1793.rabbitmqexample.consumer;
 
 import com.abdulrehman1793.rabbitmqexample.config.MessagingConfig;
 import com.abdulrehman1793.rabbitmqexample.dto.OrderStatus;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class User {
     @RabbitListener(queues = MessagingConfig.QUEUE)
     public void consumeMessageFromQueue(OrderStatus orderStatus) {
         System.out.println("Message received from queue : " + orderStatus);
+    }
+
+    @RabbitListener(queues = MessagingConfig.QUEUE)
+    public void handleMessage(Message message, Channel channel) throws IOException {
+        try {
+            byte[] payloadBytes = message.getBody();
+            String payload = new String(payloadBytes, StandardCharsets.UTF_8);
+
+            System.out.println("Message acknowledge :" + payload);
+            // Process the payload (message) and store data to the database
+            // If an error occurs, throw an exception
+
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); // Acknowledge the message
+        } catch (Exception e) {
+            // Handle the error and decide whether to acknowledge or reject the message
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true); // Reject and requeue the message for retry
+        }
     }
 }
